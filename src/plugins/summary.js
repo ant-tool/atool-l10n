b33e90385848e8fc18dc101047681c219ddf6657
W.Y <bubkoo.wy@gmail.com>
2016-11-10 12:45:13 +0800

    summary support array

--- src/plugins/summary.js
+++ src/plugins/summary.js
@@ -2,15 +2,22 @@ import { sync } from 'glob';
 import { join } from 'path';
 import log from 'spm-log';
 
+function parseMeta(cwd, sourcePattern) {
+  return sync(join(cwd, sourcePattern))
+    .map(file => require(file))
+    .reduce((a, b) => a.concat(b));
+}
+
 export default function summary(query) {
   const { sourcePattern } = {
     sourcePattern: 'i18n-messages/**/*.json',
     ...query,
   };
   log.info('summary', sourcePattern);
-  const metaArray = sync(join(this.context.cwd, sourcePattern))
-    .map(file => require(file))
-    .reduce((a, b) => a.concat(b));
+  const metaArray = Array.isArray(sourcePattern) ?
+    sourcePattern.map(pattern => parseMeta(this.context.cwd, pattern))
+      .reduce((a, b) => a.concat(b))
+    : parseMeta(this.context.cwd, sourcePattern);
   const meta = this.arrayToObject(metaArray, 'id');
   this._store.meta = meta;
   this._store.list = Object.keys(meta);
