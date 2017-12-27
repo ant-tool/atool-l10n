860ceea6b5b1ec7e7613558f26d27988bdaa13e3
liuxiaodong <liuxiaodong1989@gmail.com>
2017-12-27 15:47:57 +0800

    feat: youdao support 'openapi.youdao.com/api'

--- src/plugins/youdao.js
+++ src/plugins/youdao.js
@@ -1,5 +1,6 @@
 import log from 'spm-log';
 import request from 'co-request';
+import md5 from 'md5';
 import { stringify } from 'query-string';
 
 
@@ -14,16 +15,34 @@ async function words(url, params) {
       continue;
     }
   }
-  log.error('youdao', 'apikey is forbidden, apply another in http://fanyi.youdao.com/openapi?path=data-mode');
+  if (params.key) {
+    log.error('youdao', 'apikey is forbidden, apply another in http://fanyi.youdao.com/openapi?path=data-mode');
+  }
   return false;
 }
 
 export default async function youdao(query) {
-  const { apiname: keyfrom, apikey: key } = {
+  const { appkey, appsecret, from, to, apiname: keyfrom, apikey: key } = {
     apiname: 'iamatestmanx',
     apikey: '2137553564',
+    appkey: '',
+    appsecret: '',
+    from: 'zh',
+    to: 'en',
     ...query,
   };
+
+  let _from = from;
+  let _to = to;
+  if (_from === 'zh') _from = 'zh-CHS';
+  if (_from === 'en') _from = 'EN';
+  if (_to === 'zh') _to = 'zh-CHS';
+  if (_to === 'en') _to = 'EN';
+
+  // if (!appkey || !appsecret) {
+  //   log.error('youdao', 'appkey or appsecret is missing, can apply in http://ai.youdao.com');
+  //   return;
+  // }
 
   const todo = this.getTodo();
   if (!(todo.length)) {
@@ -31,23 +50,42 @@ export default async function youdao(query) {
     return;
   }
 
-  log.info('youdao', `using apiname: ${keyfrom}, apikey: ${key}`);
-  const url = 'http://fanyi.youdao.com/openapi.do';
+  let url;
+  if (appkey && appsecret) {
+    log.info('youdao', `using appkey: ${appkey}, appsecret: ${appsecret}`);
+    url = 'http://openapi.youdao.com/api';
+  } else {
+    log.info('youdao', `using apiname: ${keyfrom}, apikey: ${key}`);
+    url = 'http://fanyi.youdao.com/openapi.do';
+  }
 
   for (const id of todo) {
-    const texts = this.getOptionValues(id, 'zh');
+    const texts = this.getOptionValues(id, from);
+    const salt = Date.now();
     if (!texts.length) {
       log.warn('youdao', `skip ${id} from zh to en`);
     } else {
       for (const q of texts) {
-        const result = await words(url, {
+        let params = {
           keyfrom,
           key,
           type: 'data',
           doctype: 'json',
           version: '1.1',
           q,
-        });
+        };
+        if (appkey && appsecret) {
+          const sign = md5(appkey + q + salt + appsecret);
+          params = {
+            appKey: appkey,
+            salt,
+            from: _from,
+            to: _to,
+            sign,
+            q,
+          };
+        }
+        const result = await words(url, params);
         result.forEach(each => {
           log.info('youdao: zh -> en', `${q} -> ${each}`);
           this.setOption(id, 'en', {
